292d512110cedc7823bff693ed11bd2bfe65a536
Kijun Seo <kijunseo@gmail.com>
2023-07-05 08:50:46 +0900

    Initial worm

---
+++ script.js
@@ -0,0 +1,300 @@
+let text = `
+6/19/2023
+encoded and recoded and decoded
+software bots, intelligent agents, and viral code
+interconnectedness on a whole new ecological level
+futuristically recasting the planet as a net, and flesh as data
+Scott Bukatmans Terminal Identity and Mark Derys Escape Velocity)
+history of religion was really just a part of the history of media
+dark fiber, and vaguely lawless evocations of an electronic frontier
+minutiae of our DNA and the cosmological contours of deep space wormholes
+pagan and the paranormal to further colonize the twilight zones of pop media
+simulation anyway. If the posthuman future seemed so close, so technological, so secular—
+near-future cities, our media screens morphed before our eyes, as digital media remediated TV
+we build castles out of the shifting sands of consciousness and proclaim them stable, real, and eternal
+Self-help maestros and corporate consultants promulgate New Age therapies while the power elite guzzle ayahuasca and strains of Buddhism and yoga, both scientific and healthy-minded, seep through society at large
+They reflect the fact that people inhabiting all frequencies of the socioeconomic spectrum are intentionally reaching for some of the oldest navigational tools known to humankind: sacred ritual and metaphysical speculation, spiritual regimen and natural spell
+As a pagan dabbler, I grokked that the hermetic and magical fabulations that had gone underground in the modern West had returned, like Freuds repressed hankerings, in technological forms both built and imagined, demonic and transcendent, sublime and ridiculous
+Where the religious impulse cropped up could be in the most unexpected of places. Thus, nothing is off limits in TechGnosis: LSD research, the history of cybernetics, ancient aliens, sensory-deprivation tanks, Philip K. Dick, genetic engineering, Mondo 2000 magazine, religious cults, Gaia, computer viruses, nineteenth-century electromagnetism, cyberpunk, Scientology, ancient Egypt, Renaissance magic, G. I. Gurdjieff, raves, Marshall McLuhan, technopaganism, Pierre Teilhard de Chardin, Manichaeism, VR, and the Corpus Hermeticum
+
+an effort to separate the Atman (the Reality) from the non-Atman (the apparent)
+wave which brings with it an increased degree of ignorance, addiction and bondage
+First, we have to overcome the painful thought-waves by raising waves which are not painful
+the peace of the Atman within all things, even within the appearance of misery, disease, strife and want
+The Hindu ritual which corresponds most nearly to the Mass or Lord's Supper is extremely elaborate, and its performance requires almost unbroken attention
+There are also minds which, though they possess a certain degree of energy, can only dwell on what is pleasant; they shrink away from the disagreeable aspects of life
+Such expressions as the spirit of democracy, the American way of life, and so forth, bear rich crops of verbal delusion every year, in the newspapers and over the radio
+Only much later, when the painful thought--waves have been completely stilled, can we proceed to the second stage of discipline; the stilling of the not painful waves which we have deliberately created
+Even what Western psychologists call introspection or self-knowledge is objective knowledge according to Patanjali, since the mind is not the seer, but only an instrument of knowledge, an object of perception like the outside world
+Manas is the recording faculty which receives impressions gathered by the senses from the outside world. Buddhi is the discriminative faculty which classifies these impressions and reacts to them. Ahamkar is the ego-sense which claims these impressions for its own and stores them up as individual knowledge
+The real Self, the Atman, remains forever outside the power of thought-waves, it is eternally pure, enlightened and free—the only true, unchanging happiness. It follows, therefore, that man can never know his real Self as long as the thought-waves and the ego-sense are being identified. In order to become enlightened we must bring the thought-waves under control, so that this false identification may cease. The Gita teaches us that Yoga is the breaking of contact with pain
+
+long take
+non-rational cut
+Sculpting in Time
+action and empathy
+Time becomes story
+phenomenon passing through time
+the movement away from narrative
+Béla Tarr and Lav Diaz and Pedro Costa
+Lav Diaz, Béla Tarr, and Tsai Ming-liang
+Its the form of things that makes you free
+The film artist molds introspection via duration
+Two intercut moving photographs create narrative
+Time exerts a pressure which runs through the shot
+His theoretical writings echo his journey as a director
+When you hold a chord for a long time it becomes meditative
+being slow to create mood and being slow to activate the viewer
+The train journey places emphasis on expectation rather than presence
+Hollywood specialized in action (the chase), the Soviets in empathy (montage)
+This is time, time itself . . . a direct time-image which gives change unchanging form
+Duration can peel back the social veneer of an activity. Duration can invoke the Wholly Other
+Slow cinema works against the grain of cinema itself. It turns its back on what movies do best
+Tarkovsky used film techniques to study time. For Tarkovsky time was not a means to a goal. It was the goal
+I sensed a bridge between the spirituality I was raised with and the profane cinema I loved. And it was a bridge of style, not content
+It replaces action with stillness, empathy with distance. The techniques of slow cinema are all, to varying degrees, distancing devices
+three different branches of slow cinema can be seen to move away from narrative in three different directions, each with a different destination
+Her essay on Robert Bresson in Against Interpretation (1966) and the Aesthetics of Silence in Styles of Radical Will (1967) jolted me into thought
+Tarkovsky realized that what he was really after was more akin to boredom (my choice of word, not Tarkovskys) than slowness. He called it time pressure
+The film-maker, instead of creating a film world in which the viewer needs only to surrender, creates a world which the spectator must contemplate—or reject out of hand
+Postwar film-makers realized that just as movement-image could be manipulated to create suspense, time-image could be manipulated to create introspection. We not only fill in the blanks, but we create new blanks
+How does time make itself felt in a shot? Tarkovsky wrote. It becomes tangible when you realize, quite consciously, that what you see in the frame is not limited to its visual depiction but a pointer to something stretching beyond the frame into infinity
+The photographic image is the object itself. The object freed from the conditions of time and space that govern it. Viewed from this perspective, the cinema is objectivity in time. Now, for the first time, the image of things is the image of their duration
+Another way to put it: Deleuze feels that mature cinema (post-WWII) was no longer primarily concerned with telling stories to our conscious selves but now also seeks to communicate with the unconscious and the ways in which the unconscious processes memories, fantasies, and dreams
+The Tarkovsky long shot is more than long. Its meditative. The psychological effect of slow cinemas long take is unlike any other film technique. Film techniques are about getting there—telling a story, explaining an action, evoking an emotion—whereas the long take is about being there
+By delaying edits, not moving the camera, forswearing music cues, not employing coverage, and heightening the mundane, transcendental style creates a sense of unease the viewer must resolve. The film-maker assists the viewers impulse for resolution by the use of a Decisive Moment, an unexpected image or act, which then results in a stasis, an acceptance of parallel reality—transcendence
+viewers. After was slow cinema underwritten by arts organizations. Tarkovsky was not a pure slow cinema stylist—he was more interested in poetry than stasis—but he made slow cinema fashionable. He made Béla Tarr possible. WHAT IS SLOW CINEMA? Slow cinema is a fairly recent term used to designate a branch of art cinema which features minimal narrative, little action or camera movement and long running times. Harry Tuttle listed the four criteria for slow cinema as plotlessness, wordlessness, slowness, and alienation
+Chantal Akerman, Lisandro Alonso, Theo Angelopoulos, Nuri Bilge Ceylan, Pedro Costa, Claire Denis, Lav Diaz, Bruno Dumont, Michelangelo Frammartino, Hou Hsaio-hsien, Abbas Kiarostami, Kim Ki-duk, Hirokazu Kore-eda, Nicolás Pereda, Kelly Reichardt, Ben Rivers, Alberto Serra, Alexander Sokurov, Jean-Marie Straub and Danièle Huillet, Béla Tarr, Tsai Ming-liang, Apichatpong Weerasethakul. These are very different film-makers with very different artistic intentions. Yet they all use slow cinema techniques. What unites them is time
+
+proportionately tiny
+constructive opportunities in every situation
+secret to simple living is a simple, well-wishing heart
+have no way to repay your love. I can only give you myself
+Try to find how many things you have That money cannot buy
+Contentment lives only in the hearts of those who are grateful
+a true yogi will find the good in others and disregard the rest
+its the nature of one who loves the Supreme to be compassionate
+One should be like salt, like a chicken, like a crane, and like you
+So the mind is something to be used by the self, not the other way around
+it will direct body, mind, and intelligence to help us live an enlightened life
+If we allow our material ego to take charge, the atma will enter a dreamlike state
+Like the crane, they focus on bigger issues and release smaller ones. Their reward?
+How can we transcend like that? Begin by transforming your perceptions and attitudes
+she now introduces him as her eldest son and sometimes adds, Hes a very naughty son, indeed
+Your willingness to endure difficulties in service to others is the nature of a true seeker
+give the mind a more fulfilling and beneficial focus, we can easily overcome its distractions
+Rather, true bhakti is motivated by the desire to please the Supreme Beloved through loving service
+To paraphrase Albert Einstein, We cannot solve our problems with the same thinking we used to create them
+If were grateful, patient, and faithful during bright periods and dark, the light of grace will shine upon us
+well spend all our time between birth and death suffering and enjoying what we can, but well never find peace
+Although it may seem that lying is a way out of unwanted complications, telling lies taints relationships and ones own heart
+essence of our spirituality and its practices and focus on their true goal, leaving aside the unnecessary distractions of sectarianism
+We dont have to kill the ego to become humble; we have to realize our real ego, or self, by liberating it from the false ego and be true to ourselves
+holy life should not eat too much or too little or sleep too much or too little. They should give proper time to work, recreation, and their spiritual practices
+She recognized him: her merciless husband. He asked her forgiveness and she forgave him. She also gave him shelter in the orphanage, not as his wife but as a mother
+The practical lesson in all of this is that no matter what challenges await us, how we choose to respond to them determines our destiny in this life and in the next
+Higher than dull matter are the senses. Higher than the senses is the mind. Higher than the mind is the intelligence. Higher than the intelligence is the immortal self
+One whose happiness is within, who is active and rejoices within and whose aim is within, is actually a perfect mystic. Such a person ultimately attains the Supreme. —BHAGAVAD GITA 5.24
+Like salt, true yogis serve without wanting recognition or praise. They are happy to give credit to others and interested simply in doing their best to give pleasure to the Divine and benefit others
+impossible. To love you have to climb out of the cradle, where everything is getting, and grow up to the maturity of giving, without concern for getting anything special in return. —THOMAS MERTON, LOVE AND LIVING
+Regular study of sacred texts and association with sincere practitioners nourish us and provide a perspective that allows us to contemplate the nature of the self and view every aspect of our lives through the lens of timeless wisdom
+the more we expand our consciousness, the smaller we feel, because when we see ourselves in truth, the actual definition of humility, we realize were just a small, loving part of God, interconnected with the whole creation, and the servant of all beings
+she would become a mother to homeless, orphaned children. She began to collect them around her. To feed them, she sang and begged for money on railway platforms. She protected them while they slept in whatever place they could find that was relatively safe
+The heart is like a field, and wisdom like a seed. A seed will grow only in a field that has been properly tilled and fertilized. We till the field of the heart through humility, gratitude, and service to others, remaining eager to learn despite obstacles. And love grows alongside wisdom when we cultivate these qualities
+Consider the Himalayan musk deer. The male is born with a gland that, when mature, releases one of the most treasured fragrances in the world: musk. The deer, having smelled this alluring aroma produced by its own body, roams the forest seeking its source. In this fruitless quest, the deer never realizes that what its looking for is actually within itself. We are very much like that deer
+Every morning I sit down to meditate on the divine sound of my mantra. Anyone who meditates knows what comes next: my mind races through whatever I struggled with yesterday (or will grapple with today) or, if not my problems, then the problems of my loved ones, or it chews on an old argument, or gets lost in memory (the joy of the first Chevrolet I ever owned, or the shame of striking out and losing the championship game for my Little League baseball team, for example). If someone is baking in the next room, I can switch in an instant from meditation on my mantra to meditation on cookies. At some point, Ill become aware of the wandering mind and ask myself, What happened to the mantra?`
+let textArr = text.split('\n');
+
+console.log(textArr);
+let height = textArr.length;
+let width = textArr[height-1].length;
+function render() {
+  let board = document.getElementById("game-board");
+  board.innerHTML = "";
+  for (let x of textArr) {
+    
+    for (let c of x) {
+      const para = document.createElement("span");
+      const node = document.createTextNode(c);
+      para.appendChild(node);
+      board.appendChild(para);
+    }
+    const para = document.createElement("span");
+    const node = document.createTextNode(" ");
+    para.appendChild(node);
+    board.appendChild(para);
+  }
+}
+render();
+
+let x = -1;
+let y = -1;
+let ref = "█"
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function isLetter (_x, _y) {
+  if (_y >= height || _y < 0 || _x < 0 || _x >= width) return false;
+  let line = textArr[_y];
+  if (line.length <= _x) return false;
+  let char = line[_x];
+  if (char == " ") return false;
+  if (char == ref) return false;
+  return true;
+}
+
+function setCharAt(str,index,chr) {
+    if(index > str.length-1) return str;
+    return str.substring(0,index) + chr + str.substring(index+1);
+}
+
+function replaceLetter(_x, _y) {
+  let line = textArr[_y];
+  textArr[_y] = setCharAt(line, _x, ref);
+}
+
+function findStarting() {
+  while (!isLetter(x, y)) {
+    x = getRandomInt(width);
+    y = getRandomInt(height);
+  }
+}
+
+findStarting();
+console.log(x);
+console.log(y);
+
+
+// for every snake
+
+function hasContent(node) {
+  return (node && node.textContent != ' ' && node.tagName == "SPAN" && node.style.backgroundColor != "black") ? node : null;
+}
+
+const INTERVAL = 20;
+
+function moveSnake2() {
+  let sx = getRandomInt(window.innerWidth);
+  let sy = getRandomInt(window.innerHeight);
+
+  function _moveSnake() {
+    let a = document.elementFromPoint(sx, sy)
+    if (hasContent(a)) {
+      a.style.backgroundColor = "black";
+      let right = a.nextSibling;
+      let left = a.previousSibling;
+      let rect = a.getBoundingClientRect();
+      // try to move right
+      if (hasContent(right)) {
+        //console.log("right");
+        rect = right.getBoundingClientRect();
+        sx = rect.x + rect.width/2;
+        sy = rect.y + rect.height/2;
+        //_moveSnake();
+        setTimeout(_moveSnake, INTERVAL);
+      } else if (hasContent(left)) {
+        //console.log("left");
+        rect = left.getBoundingClientRect();
+        sx = rect.x + rect.width/2;
+        sy = rect.y + rect.height/2;
+        //_moveSnake();
+        setTimeout(_moveSnake, INTERVAL);
+      }
+      // else try to go top
+      else {
+        //console.log("up or down");
+        let up = document.elementFromPoint(rect.x - rect.width/2, rect.y - rect.height/2);
+        let down = document.elementFromPoint(rect.x + rect.width, rect.y + rect.height);
+        
+        if (Math.random() > 0.5) {
+          if (hasContent(up)) {
+            sx = rect.x - rect.width/2;
+            sy = rect.y - rect.height/2;
+            //_moveSnake();
+            //console.log("up");
+            setTimeout(_moveSnake, INTERVAL);
+          } else if (hasContent(down)) {
+            sx = rect.x + rect.width;
+            sy = rect.y + rect.height;
+            //_moveSnake();
+            //console.log("down");
+            setTimeout(_moveSnake, INTERVAL);
+          } else {
+            // else we are dead
+            //console.log("we dead");
+            setTimeout(moveSnake2, INTERVAL*2);
+          }
+        } else {
+          if (hasContent(down)) {
+            sx = rect.x + rect.width;
+            sy = rect.y + rect.height;
+            //_moveSnake();
+            //console.log("down");
+            setTimeout(_moveSnake, INTERVAL);
+          } else if (hasContent(up)) {
+            sx = rect.x - rect.width/2;
+            sy = rect.y - rect.height/2;
+            //_moveSnake();
+            //console.log("up");
+            setTimeout(_moveSnake, INTERVAL);
+          } else {
+            // else we are dead
+            //console.log("we dead");
+            setTimeout(moveSnake2, INTERVAL*2);
+          }
+        }
+      }
+    }
+    else {
+      console.log("no content");
+      setTimeout(moveSnake2, INTERVAL*2);
+    }
+  }
+  _moveSnake();
+}
+
+function moveSnake(){
+//  findStarting();
+  replaceLetter(x,y);
+  render();
+  
+  let nextX = x;
+  let nextY = y;
+  function findNext() {
+    if (isLetter(x+1, y)) {
+      nextX += 1;
+      return true;
+    } else if (isLetter(x-1, y)) {
+      nextX -= 1;
+      return true;
+    } else if (isLetter(x, y+1)) {
+      nextY += 1;
+      return true;
+    } else if (isLetter(x, y-1)) {
+      nextY -= 1;
+      return true;
+    }
+    return false;
+  }
+  if (findNext()) {
+    x = nextX;
+    y = nextY;
+  } else {
+    findStarting();
+  }
+  
+  //setTimeout(moveSnake, 10000);
+  /*
+  if (Math.random() > 0.5) {
+    setTimeout(moveSnake, getRandomInt(500));
+    setTimeout(moveSnake, getRandomInt(500));
+  } else if (Math.random() > 0.5) {
+    setTimeout(moveSnake, getRandomInt(500));
+  }*/
+}
+
+for (let i = 0; i < 1; i++) {
+  moveSnake2();
+}
